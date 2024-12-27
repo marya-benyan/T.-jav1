dd6e2b51a00cf2be92ef7cc0909b4999fc4cd7a4
marya-benyan <maryabenyan@gmail.com>
2024-12-27 22:05:36 +0300

    your message

---
+++ jav1.js
@@ -0,0 +1,54 @@
+//  Q1
+
+console.log(-5 * 3);
+
+console.log("JavaScript " + 50);
+
+console.log(17 % 5);
+
+console.log(5 % 17);
+
+console.log(5 / 10);
+
+console.log(4 === '4');
+
+console.log(4 != 5);
+
+console.log(7 <= 8);
+
+console.log("Hello" + 5);
+
+let x = 5
+console.log(Math.ceil(x) - Math.floor(x))
+
+console.log(Math.pow(x, 2));
+
+//  Q2
+
+// let number = prompt("Enter a number:");
+// alert("You entered: " + number);
+
+// Q3
+// let num1 = parseInt(prompt("Enter the first number:"));
+// let num2 = parseInt(prompt("Enter the second number:"));
+// let sum = num1 + num2;
+// alert("The sum is: " + sum);
+
+// Q4
+//  let num1 = parseInt(prompt("Enter the first number:"));
+//  let num2 = parseInt(prompt("Enter the second number:"));
+//  let PRD= num1 * num2;
+//  alert("The PRODUCT is: " + PRD);
+
+// Q5
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+// let DIV = num1 / num2;
+// alert("The DIV is: " + DIV);
+
+// Q6
+
+// let sum = num1 + num2;
+let num1=5;
+let num2=3;
+alert(`the sum of ${num1} and ${num2} is ${num1+num2}`);
